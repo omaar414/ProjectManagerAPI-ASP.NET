89318a4349ede38acd6bc15436bdec23d16f34ef
omaar414 <omaar414@gmail.com>
2025-03-19 21:41:45 -0400

    Added Project entity

---
+++ ProjectManager.API/Domain/Entities/Project.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager.API.Domain.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int OwnerId { get; set; } 
+        
+    }
+}
