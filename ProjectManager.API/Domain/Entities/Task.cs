b97c7abc07286ac555867f98c7c6170ec28683fb
omaar414 <omaar414@gmail.com>
2025-03-20 00:38:03 -0400

    practica

---
+++ ProjectManager.API/Domain/Entities/Task.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManager.API.Domain.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
